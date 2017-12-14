44fb6ee5aa79c231b894e09013ce57d7e5e989ed
Unknown <smm_101995@outlook.es>
2017-12-14 22:01:59 +0100

    Actualizado

--- src/com/cice/ejercicios/Ejercicio2.java
+++ src/com/cice/ejercicios/Ejercicio2.java
@@ -3,7 +3,19 @@ package com.cice.ejercicios;
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String prueba="pepe juan manolo ricardo";
+		
+		int y=prueba.indexOf(" ");
+		System.out.println(y);
+		
+		String pruebaRen=prueba.replace(" ", "/ ");
+		
+		for(int i=0;i<prueba.length();i++) {
+			System.out.println(prueba.indexOf(" ",i));
+		}
+		
+		System.out.println(pruebaRen);
 
 	}
 
